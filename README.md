e2da114e0b3a844e6bfa59b3ed2a89fc4b375b26
bchao1 <whereisbrianchao520@gmail.com>
2020-07-31 22:10:49 +0800

    more results

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 |Before|After|Procedure|
 |--|--|--|
 |![img](./examples/castle/before.jpg)|![img](./examples/castle/after.jpg)|![img](./examples/castle/demo.gif)|
+|![img](./examples/lake/before.jpg)|![img](./examples/lake/after.jpg)|![img](./examples/lake/demo.gif)|
+|![img](./examples/tree/before.jpg)|![img](./examples/tree/after.jpg)|![img](./examples/tree/demo.gif)|
--- main.py
+++ main.py
@@ -1,29 +1,52 @@
+import os
 import numpy as np
 from PIL import Image
 import utils
 
-scale = 0.25
-pil_img = Image.open('castle.jpg')  # read in image
+exp = 'examples/tree'
+imfile = 'tree.jpg'
+vertical = True
+
+# Downscale image (just for faster demo)
+scale = 0.5
+pil_img = Image.open(os.path.join(exp, imfile))  # read in image
 pil_img.thumbnail((pil_img.size[0] * scale, pil_img.size[1] * scale), Image.ANTIALIAS)
 
+# Cast to numpy
 color_img = np.array(pil_img)  # cast to numpy array (h * w * c)
+
+if vertical:
+    color_img = np.transpose(color_img, (1, 0, 2))
 
 carved = [pil_img]
 
 print("Before, ", color_img.shape)
-Image.fromarray(color_img).save('before.jpg')
+
+save_img = color_img
+if vertical:
+    save_img = np.transpose(color_img, (1, 0, 2))
+Image.fromarray(save_img).save(os.path.join(exp, 'before.jpg'))
 
 for i in range(100):
     print("Seam carving {} ...".format(i))
     color_img = utils.seam_carve(color_img)
     pad = np.ones((color_img.shape[0], i + 1, 3)) * 255
     save_img = np.concatenate((color_img, pad.astype(np.uint8)), axis=1)
+    
+    if vertical:
+        save_img = np.transpose(save_img, (1, 0, 2))
+
     carved.append(Image.fromarray(save_img))
 
 print("After, ", color_img.shape)
-Image.fromarray(color_img).save('after.jpg')
 
-carved[0].save('demo.gif', format='GIF', append_images=carved[1:], save_all=True, duration=100, loop=0)
+save_img = color_img
+if vertical:
+    save_img = np.transpose(color_img, (1, 0, 2))
+Image.fromarray(save_img).save(os.path.join(exp, 'after.jpg'))
+
+carved[0].save(os.path.join(exp, 'demo.gif'), 
+    format='GIF', append_images=carved[1:], save_all=True, duration=100, loop=0)
 
 
 
